22e68589935437358649afd9e96ec2c4f317d35d
Steven Mokoena <steven@sizananitechnologies.com>
2022-05-21 08:02:56 +0200

    first commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new System.Collections.Generic.List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+            /*
+             * can be either:
+             *  View()
+             *  PartialView()
+             *  Content()
+             *  Redirect()
+             *  RedirectToAction()
+             *  Json()
+             *  File()
+             *  HttpNotFound()
+             *  new EmptyResult()
+             */
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public IActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
